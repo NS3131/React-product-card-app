447b9bee5ce7e8ba33c3c7dab750e5616cb0d5ee
NS3131 <shaumba@hotmail.com>
2025-09-05 19:54:49 +0100

    modify product

--- src/data/product.js
+++ src/data/product.js
@@ -1,8 +1,8 @@
 const product = {
-  name: "Galaxy Smartwatch",
-  price: "$199.99",
-  description: "A sleek and modern smartwatch with all-day battery life.",
-  image: "https://via.placeholder.com/300x200.png?text=Galaxy+Watch"
+  name: "Chanel watch",
+  price: "$1999.99",
+  description: "A sleek and modern elegant watch with for ladies.",
+  image: "https://via.placeholdehttps://www.chisholmhunter.co.uk/media/catalog/product/cache/1c13339936d9cec19ec06c7ff2acbcef/2/-/2-117-01-0014_p3.jpgr.com/300x200.png?text=Galaxy+Watch"
 };
 
 export default product;
